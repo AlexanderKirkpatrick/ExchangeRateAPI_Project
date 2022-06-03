a1e226c69f498a1ba5fa34b967b4261d2b15df36
Alexander <knibz@protonmail.com>
2022-06-03 15:22:32 -0700

    fix typos

--- src/exchanger.js
+++ src/exchanger.js
@@ -1,9 +1,9 @@
 export class ExchangeRate {
-  async getExchangeRate(baseCurrency, targetCurrency) {
+  async getExchangeRates(baseCurrency, targetCurrency) {
     try {
       let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCurrency}/${targetCurrency}`);
       let jsonResponse;
-      if (repsonse.ok && response.status == 200) {
+      if (response.ok && response.status == 200) {
         jsonResponse = await response.json();
         return jsonResponse;
       }
