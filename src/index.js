a93ab68801c0be1f14811efcd4c0b8a8aa41bf0b
Alexander <knibz@protonmail.com>
2022-06-03 10:52:44 -0700

    add conversionDisplay function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import {CurrencyCalculator} from './currencyCalculator';
+
+function conversionDisplay(currencyExchanger, convertedAmount) {
+  $('.base-currency-display').text(currencyExchanger.baseCurrency);
+  $('.base-amount-display').text(currencyExchanger.baseAmount);
+  $('.target-currency-display').text(currencyExchanger.targetCurrency);
+  $('.exchange-rate-display').text(currencyExchanger.exchangeRate);
+  $('.converted-currency').text(convertedAmount.toString());
+
+  $('#error').hide();
+  $('#result').show();
+}
+
