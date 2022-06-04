8b84e9b1d9b95042f68bd3625b87d9ccd074166d
Alexander <knibz@protonmail.com>
2022-06-03 23:41:08 -0700

    edit api link standard from pair

--- src/exchanger.js
+++ src/exchanger.js
@@ -1,7 +1,7 @@
 export class ExchangeRate {
-  async getExchangeRates(baseCurrency, targetCurrency) {
+  async getExchangeRates(baseCurrency) {
     try {
-      let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCurrency}/${targetCurrency}`);
+      let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCurrency}`);
       let jsonResponse;
       if (response.ok && response.status == 200) {
         jsonResponse = await response.json();
