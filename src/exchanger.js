e3456dbb208bb6b02c73938cad303ebdcee519d8
Alexander <knibz@protonmail.com>
2022-06-04 21:55:28 -0700

    edited syntax, but changed it back

--- src/exchanger.js
+++ src/exchanger.js
@@ -3,11 +3,12 @@ export class ExchangeRate {
     try {
       let response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCurrency}`);
       let jsonResponse;
+      
       if (response.ok && response.status == 200) {
         jsonResponse = await response.json();
         return jsonResponse;
       }
-      return jsonResponse;
+      return await jsonResponse;
     } catch (error) {
       return error;
     }
