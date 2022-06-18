861ae525ef8b7c264459bee4e21857262a8fd85a
Alexander <knibz@protonmail.com>
2022-06-18 11:57:52 -0400

    edit url from latest to pair, allow for targetCurrency argument to be passed. Also edit Error throw from statusText to status

--- src/exchanger.js
+++ src/exchanger.js
@@ -1,17 +1,18 @@
 export class ExchangeRate {
-  static async getExchangeRates(baseCurrency) {
+  static async getExchangeRates(baseCurrency, targetCurrency) {
     try {
-      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/${baseCurrency}`);
+      const response = await fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/${baseCurrency}/${targetCurrency}`);
       
       if (!response.ok) {
-        throw Error(response.statusText);
+        throw Error(response.status);
       }
-      return response.json();
+      return response.json();      
     } catch(error) {
       return error.message;
     }
   }
 }
+
 
 
 
